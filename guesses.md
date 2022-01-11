62b0423da9056df5408a03febf5ebc964ef1d6f4
Matt Whelan <matt@blacklogik.com>
2022-01-10 20:50:27 -0800

    guesses.md, and print frequency analysis

---
+++ guesses.md
@@ -0,0 +1,40 @@
+Example output:
+
+    Letter | Count
+    a      | 4467
+    e      | 4254
+    r      | 3043
+    o      | 2798
+    i      | 2581
+    s      | 2383
+    t      | 2381
+    l      | 2368
+    n      | 2214
+    u      | 1881
+    y      | 1605
+    c      | 1546
+    d      | 1399
+    h      | 1323
+    m      | 1299
+    p      | 1291
+    b      | 1160
+    g      | 1102
+    k      | 882
+    w      | 685
+    f      | 661
+    v      | 466
+    z      | 250
+    x      | 189
+    j      | 163
+    q      | 84
+    
+    arose
+    until
+    duchy
+    plumb
+    gawky
+    vifda
+    azoxy
+    jiqui
+
+I'm pretty happy with the first three words; they cover 14 distinct letters, and the most frequent 14 (in this word list, at least). 'plumb' is pretty efficient too, with just two letter repeats. It's unlikely one is groping blindly by guess four, so the value decreases. 
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ use std::fs::File;
 use std::io::Read;
 use std::process;
 
+const TARGET_WORD_LEN: usize = 5;
+
 fn main() -> Result<(), anyhow::Error> {
     let arguments: Vec<String> = args().collect();
     if arguments.len() < 2 {
@@ -23,24 +25,38 @@ fn main() -> Result<(), anyhow::Error> {
         let all_words: Vec<&str> = contents.lines().collect();
 
         // Filter out proper names, words of the wrong length, and disallowed words
-        let five_words: Vec<&str> = all_words.iter()
+        let target_words: Vec<&str> = all_words.iter()
             .filter(|s| s.chars().all(|ch| ch.is_lowercase()))
-            .filter(|s| s.len() == 5)
+            .filter(|s| s.len() == TARGET_WORD_LEN)
             .filter(|s| !disallowed.contains(**s))
             .copied()
             .collect();
 
         // Frequency analysis. Characters are worth points equal to their number of appearances in the word list
-        let freq = char_freq(&five_words);
+        let freq = char_freq(&target_words);
+
+        show_freq(&freq);
+
         // Find the words which give you the best character coverage
         let mut coverage= HashSet::new();
-        let guesses = best_guesses(&five_words, &freq, &mut coverage);
+        let guesses = best_guesses(&target_words, &freq, &mut coverage);
         for guess in guesses {
             println!("{}", guess);
         }
 
         Ok(())
     }
+}
+
+fn show_freq(freq: &HashMap<char, usize>) {
+    let mut by_freq = Vec::from_iter(freq.iter());
+    by_freq.sort_by_key(|(_, v)| *v);
+    by_freq.reverse();
+    println!("Letter | Count");
+    for (letter, count) in by_freq {
+        println!("{:7}| {}", letter, count);
+    }
+    println!()
 }
 
 fn best_guesses<'a>(words: &'a [&str], freq: &HashMap<char, usize>, covered: &mut HashSet<char>) -> Vec<&'a str> {
